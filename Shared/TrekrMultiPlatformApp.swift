7264a04a526158883ead5c1f949018222c92a064
Diego Sierraalta <dsierraalta@lcs.on.ca>
2022-01-18 09:24:28 -0500

    Initial Commit

---
+++ Shared/TrekrMultiPlatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrMultiPlatformApp.swift
+//  Shared
+//
+//  Created by Diego Sierraalta on 2022-01-18.
+//
+
+import SwiftUI
+
+@main
+struct TrekrMultiPlatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
