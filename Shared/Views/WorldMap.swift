1e9bbeeb41e61394890b802a77828447ad040586
Diego Sierraalta <dsierraalta@lcs.on.ca>
2022-01-25 09:07:53 -0500

    Added navigation link to the map view.
    This makes the map clickable

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -18,27 +18,33 @@ struct WorldMap: View {
     // Center on LCS, wide enough to show most of eastern North America
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922,
                                                                           longitude: -78.26571),
-                                                                          span: MKCoordinateSpan(latitudeDelta: 40,
-                                                                                                  longitudeDelta: 40))
+                                           span: MKCoordinateSpan(latitudeDelta: 40,
+                                                                  longitudeDelta: 40))
     
     var body: some View {
-
+        
         Map(coordinateRegion: $region, annotationItems: store.places) { location in
             
             MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: location.latitude,
                                                              longitude: location.longitude)) {
                 
-                Image(location.country)
-                    .resizable()
-                    .cornerRadius(10)
-                    .frame(width: 80, height: 40)
-                    .shadow(radius: 3)
+                NavigationLink(destination: LocationDetail(location: location)) {
+                    
+                    Image(location.country)
+                        .resizable()
+                        .cornerRadius(10)
+                        .frame(width: 80, height: 40)
+                        .shadow(radius: 3)
+                    
+                    
+                }
+                
                 
             }
             
         }
-            .navigationTitle("Map")
-
+        .navigationTitle("Map")
+        
     }
 }
 
@@ -49,4 +55,4 @@ struct WorldMap_Previews: PreviewProvider {
         }
     }
 }
- 
+
